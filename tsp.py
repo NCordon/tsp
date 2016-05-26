a3399eafde30049b11de2808f0b921078a676e0e
NCordon <nacho.cordon.castillo@gmail.com>
2016-05-26 04:18:22 +0200

    Añadida documentación inicial

--- tsp.py
+++ tsp.py
@@ -3,26 +3,48 @@
 
 # # Travelling Salesman Problem
 
-# In[1]:
+# El problema consiste en modelar un problema de viajante de comercio con
+# resolución aproximada mediante las metaheurísticas *Enfriamiento Simulado*
+# y *Búsqueda Tabú*
+
+# In[62]:
 
 get_ipython().magic(u'matplotlib inline')
 
 
-# In[2]:
+# In[39]:
 
 from numpy import *
 from random import *
-from math import exp
+from math import exp, log
 from copy import deepcopy
 from itertools import cycle
-from collections import deque
 import matplotlib as matplotlib
 import re
 
 
-# Definimos una solución como una permutación con su coste
+# Definimos una solución como un objeto `Route`.
+# 
+# > **`Route`**
+# >> Recibe en su construcción una permutación y una matriz de distancias
+# 
+# >> **`permutation`**: Contiene la permutación de ciudades en el orden en que
+# se visitan
+# 
+# >> **`cost`**: Devuelve el coste de la permutación
+# 
+# >> **`update_cost`**: Recalcula el coste de la permutación
+# 
+# >> **`change_positions`**: Método que intercambia dos ciudades de la
+# permutación
+# 
+# >> **`change_edges`**: Método que permite intercambiar dos arcos de la permutación dados por su índice, donde
+# si la permutación es [1,3,4], el arco 0-ésimo es (4,1) y el arco 1-ésimo es (1,3)
+# 
+# >> **`get_edges`**: Método que devuelve los arcos como tuplas de las ciudades que unen
+# 
 
-# In[3]:
+# In[40]:
 
 class Route:
         
@@ -57,14 +79,19 @@ class Route:
             
 
 
-# Implementación de la clase que albergará los datos del problema
+# Implementación de la clase `TSP` que albergará los datos del problema
 
-# In[223]:
+# In[187]:
 
-class TSP:   
+class TSP:
+    def __init__(self, file):
+        self.points = array(self.__read(file))
+        self.dist = sqrt(
+            [
+                [dot(subtract(x,y),subtract(x,y)) for x in self.points] 
+                for y in self.points
+            ])    
     
-    #def prueba(self):
-    #    return(Route(array(range(len(self.points))), self.dist))
     
     def __read (self, file):  
         f = open(file, 'r')
@@ -75,16 +102,28 @@ class TSP:
             is_point = re.search(match, line)
 
             if is_point:
+                """ Quitamos el índice del punto """
                 x,y = map(float, line.split()[1:])
                 points.append((x,y))
     
         return(points)
     
-    
-    def simulated_annealing(self, t_ini, max_iter, alpha):
-        """Temperatura"""
-        t = t_ini
+    def print_solution(self, solution):
+        p_x = [ self.points[i][0] for i in solution.permutation ]
+        p_y = [ self.points[i][1] for i in solution.permutation ]
+        p_x = append(p_x, p_x[0])
+        p_y = append(p_y, p_y[0])
+        tol_x = 0.05 * mean(p_x)
+        tol_y = 0.05 * mean(p_y)
         
+        matplotlib.rcParams.update({'font.size': 18, 'lines.linewidth':3})
+        matplotlib.pyplot.figure(figsize=(15,10))
+        matplotlib.pyplot.xlim(min(p_x) - tol_x, max(p_x) + tol_x)
+        matplotlib.pyplot.ylim(min(p_y) - tol_y, max(p_y) + tol_y)
+        matplotlib.pyplot.plot(p_x, p_y, marker='o', color='red', markersize=7)
+        
+    
+    def simulated_annealing(self, t_ini, max_iter, alpha, mu, phi):
         """Número de ciudades"""
         n = len(self.points)
         
@@ -93,24 +132,27 @@ class TSP:
         shuffle(permutation)
         solution = Route(permutation, self.dist)
         best_solution = Route(permutation, self.dist)
+
+        """Temperatura inicial"""
+        t = (mu/-log(phi))*solution.cost        
         
         """Variables que controlan las iteraciones"""
         improvement = True
         i=0
         
+        """Operador de vecino"""
+        
+        
         while i < max_iter:
             candidate = deepcopy(solution)
             
-            # Generamos los índices de los arcos a cambiar
+            # Generación de un vecino
             u = randint(0, n-1)
             v = randint(u+1, n)   
             candidate.change_edges(u,v)
             
-            """u = randint(0, n-1)
-            v = randint(0, n-1)
-            candidate.change_positions(u,v)"""
-            
             diff_cost = candidate.cost - solution.cost
+            
             
             if (diff_cost < 0 or random() < exp(-diff_cost*1.0/t)):
                 solution = deepcopy(candidate)
@@ -182,11 +224,10 @@ class TSP:
 
                 restart = False
                 non_improving = 0                    
-                #tabu_list = [None] * int(coef_tenencia*n)
-    
-            
+                    
             best_neighbour = deepcopy(neighbour)
             u_tabu, v_tabu = None, None
+
             
             while i< max_iter and j < max_vecinos and not restart:     
                 # Generamos los índices de los arcos a cambiar
@@ -209,7 +250,6 @@ class TSP:
                 if eval_solution:
                     if candidate.cost < best_neighbour.cost:
                         best_neighbour = deepcopy(candidate)
-                        #best_list.append(best_neighbour)
                         u_tabu, v_tabu = u,v  
 
                         for (p,q) in best_neighbour.get_edges():
@@ -217,11 +257,10 @@ class TSP:
 
                         if candidate.cost < best_solution.cost:
                             best_solution = deepcopy(candidate)                      
-
+                            improvement = True
                     else:
                         non_improving += 1
-                        restart = (non_improving == limit_restart)
-                            
+                        restart = (non_improving == limit_restart)          
                   
                 j+=1
                 i+=1
@@ -238,34 +277,13 @@ class TSP:
             
         """Fin de toda la exploración"""
         return best_solution
-    
-        
-    def print_solution(self, solution):
-        p_x = [ self.points[i][0] for i in solution.permutation ]
-        p_y = [ self.points[i][1] for i in solution.permutation ]
-        p_x = append(p_x, p_x[0])
-        p_y = append(p_y, p_y[0])
-        tol_x = 0.05 * mean(p_x)
-        tol_y = 0.05 * mean(p_y)
-        
-        matplotlib.rcParams.update({'font.size': 18, 'lines.linewidth':3})
-        matplotlib.pyplot.figure(figsize=(15,10))
-        matplotlib.pyplot.xlim(min(p_x) - tol_x, max(p_x) + tol_x)
-        matplotlib.pyplot.ylim(min(p_y) - tol_y, max(p_y) + tol_y)
-        matplotlib.pyplot.plot(p_x, p_y, marker='o', color='red', markersize=7)
-    
-    def __init__(self, file):
-        self.points = array(self.__read(file))
-        self.dist = sqrt(
-            [
-                [dot(subtract(x,y),subtract(x,y)) for x in self.points] 
-                for y in self.points
-            ])
 
 
-# In[6]:
+# In[179]:
 
-files = ['berlin52.tsp', 'ch150.tsp', 'd198.tsp', 'eil101.tsp']
+files = ['berlin52.tsp', 'ch150.tsp', 'd198.tsp', 'eil101.tsp', 'rd400.tsp']
+semillas = [12345, 23451, 34512, 45123, 51234] 
+#semillas = [12345]
 
 problems = {}
 sa_solutions = {}
@@ -273,26 +291,34 @@ ts_solutions = {}
 best_solutions = {'berlin52': 7542,
                   'ch150': 6528,
                   'd198': 15780,
-                  'eil101': 629}
+                  'eil101': 629,
+                  'rd400': 15281}
+sa_costes = {'berlin52': 0.0,
+          'ch150': 0.0,
+          'd198': 0.0,
+          'eil101': 0.0,
+          'rd400': 0.0}
+ts_costes = deepcopy(sa_costes)
 
 
-# In[29]:
-
-semilla = 12345678
+# In[ ]:
 
 for f in files:
-    seed(semilla)
     name = f[:-4]
     problems[name] = TSP(f)
     size = len(problems[name].points)
     n_iter = size*100
     alpha = 0.95
-    sa_solutions[name] = problems[name].simulated_annealing(size*1e3, n_iter, alpha) 
+    
+    for s in semillas:
+        seed(s)
+        sa_solutions[name] = problems[name].simulated_annealing(size*1e3, n_iter, alpha, 0.05, 0.05) 
+        sa_costes[name] += sa_solutions[name].cost
+        
+    sa_costes[name] /= len(semillas)
 
 
-# In[240]:
-
-semilla = 12345678
+# In[181]:
 
 for f in files:
     seed(semilla)
@@ -307,15 +333,21 @@ for f in files:
     prob_bs = 0.95
     prob_greedy = 0.05
 
-    ts_solutions[name] = problems[name].tabu_search(n_iter, max_vecinos, tabu_tenencia, aspiration_tol, limit_restart)
+    for s in semillas:
+        seed(s)
+        ts_solutions[name] = problems[name].tabu_search(n_iter, max_vecinos, tabu_tenencia, aspiration_tol, limit_restart)
+        ts_costes[name] += ts_solutions[name].cost
+        
+    ts_costes[name] /= len(semillas)
+   
 
 
-# In[241]:
+# In[182]:
 
 for name in problems:
     print (name 
-           + '\n\t SA: '   + str(sa_solutions[name].cost)
-           + '\n\t TS: '   + str(ts_solutions[name].cost)
+           + '\n\t SA: '   + str(sa_costes[name])
+           + '\n\t TS: '   + str(ts_costes[name])
            + '\n\t Best: ' + str(best_solutions[name]))
     problems[name].print_solution(sa_solutions[name])
     problems[name].print_solution(ts_solutions[name])
